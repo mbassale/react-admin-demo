b6434c2aba596c375b87aadef4f6be3b9cafcf16
Marco Bassaletti <bassaletti@gmail.com>
2020-09-18 23:30:39 -0300

    PostCreate component
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
-import { PostList, PostEdit } from './components/posts';
+import { PostList, PostCreate, PostEdit } from './components/posts';
 import { UserList } from './components/users';
 import jsonServerProvider from 'ra-data-json-server';
 
@@ -8,7 +8,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} edit={PostEdit} />
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
--- src/components/posts.js
+++ src/components/posts.js
@@ -5,6 +5,7 @@ import {
     TextField,
     ReferenceField,
     EditButton,
+    Create,
     Edit,
     SimpleForm,
     ReferenceInput,
@@ -25,6 +26,18 @@ export const PostList = props => (
             <EditButton/>
         </Datagrid>
     </List>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name"/>
+            </ReferenceInput>
+            <TextInput source="title"/>
+            <TextInput multiline source="body"/>
+        </SimpleForm>
+    </Create>
 );
 
 export const PostEdit = props => (
