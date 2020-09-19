fcc1b8bd89d9898488d464b7d6189cfe4d40c53d
Marco Bassaletti <bassaletti@gmail.com>
2020-09-18 23:42:01 -0300

    Filtering capabilities to posts.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {
+    Filter,
     List,
     Datagrid,
     TextField,
@@ -16,8 +17,17 @@ import SummaryField from './SummaryField';
 
 const PostTitle = ({ record }) => (<span>{record ? `Post: ${record.title}` : ''}</span>);
 
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
 export const PostList = props => (
-    <List {...props}>
+    <List filters={<PostFilter />} {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id"/>
             <ReferenceField source="userId" reference="users">
