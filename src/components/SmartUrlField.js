83e1485d4e7a5195bc2488173b017e4309f0075d
Marco Bassaletti <bassaletti@gmail.com>
2020-09-18 22:49:38 -0300

    Styled SmartUrlField.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/SmartUrlField.js
+++ src/components/SmartUrlField.js
@@ -1,14 +1,30 @@
 import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LaunchIcon from '@material-ui/icons/Launch';
 import Link from '@material-ui/core/Link';
 
+const useStyles = makeStyles({
+    link: {
+        textDecoration: 'none'
+    },
+    icon: {
+        width: '0.5em',
+        paddingLeft: 2
+    }
+})
+
 const SmartUrlField = ({ record = {}, source }) => {
+    const classes = useStyles();
     const originalUrl = record[source];
     let url = originalUrl;
     if (!originalUrl.startsWith('https://') && !originalUrl.startsWith('http://')) {
         url = 'http://' + url;
     }
     return (
-        <Link component="a" href={url} target="_blankd">{originalUrl}</Link>
+        <Link component="a" href={url} target="_blank" className={classes.link}>
+            {originalUrl}
+            <LaunchIcon className={classes.icon} />
+        </Link>
     );
 }
 
