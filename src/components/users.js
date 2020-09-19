112b0de63d937fc299ba2eda34f14be041200b1f
Marco Bassaletti <bassaletti@gmail.com>
2020-09-19 00:06:16 -0300

    Responsive: show simple list on mobile devices.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/users.js
+++ src/components/users.js
@@ -1,16 +1,26 @@
 import * as React from 'react';
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+import { List, Datagrid, TextField, EmailField, SimpleList } from 'react-admin';
 import SmartUrlField from './SmartUrlField';
 
-export const UserList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <EmailField source="email" />
-            <TextField source="phone" />
-            <SmartUrlField source="website" />
-            <TextField source="company.name" />
-        </Datagrid>
-    </List>
-);
+export const UserList = props => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List {...props}>
+            {isSmall ?
+                (
+                    <SimpleList primaryText={record => record.name} secondaryText={record => record.phone} tertiaryText={record => record.email} />
+                ) : (
+                    <Datagrid rowClick="edit">
+                        <TextField source="id"/>
+                        <TextField source="name"/>
+                        <EmailField source="email"/>
+                        <TextField source="phone"/>
+                        <SmartUrlField source="website"/>
+                        <TextField source="company.name"/>
+                    </Datagrid>
+                )
+            }
+        </List>
+    );
+};
