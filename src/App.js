3938975a51e50f372c31a91ce36f64ba557c4dd1
Marco Bassaletti <bassaletti@gmail.com>
2020-09-18 23:24:07 -0300

    PostEdit component.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
-import { PostList } from './components/posts';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import { PostList, PostEdit } from './components/posts';
 import { UserList } from './components/users';
 import jsonServerProvider from 'ra-data-json-server';
 
@@ -8,7 +8,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,17 +1,41 @@
 import * as React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput
+} from 'react-admin';
 import SummaryField from './SummaryField';
 
 export const PostList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
-            <TextField source="id" />
+            <TextField source="id"/>
             <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
+                <TextField source="name"/>
             </ReferenceField>
-            <TextField source="title" />
-            <SummaryField source="body" />
-            <EditButton />
+            <TextField source="title"/>
+            <SummaryField source="body"/>
+            <EditButton/>
         </Datagrid>
     </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name"/>
+            </ReferenceInput>
+            <TextInput source="title"/>
+            <TextInput multiline source="body"/>
+        </SimpleForm>
+    </Edit>
 );
