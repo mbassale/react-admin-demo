1cc79fd060c3a3c2c2388e2e19892e0473e403fe
Marco Bassaletti <bassaletti@gmail.com>
2020-09-18 23:03:13 -0300

    Created a posts listing with edit button.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import { PostList } from './components/posts';
 import { UserList } from './components/users';
 import jsonServerProvider from 'ra-data-json-server';
 
@@ -7,6 +8,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
---
+++ src/components/posts.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import SummaryField from './SummaryField';
+
+export const PostList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <SummaryField source="body" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
